6b28d02c9809a1adf85b6545af5760ebc8c70cb9
Ben Kempner <ben@verticalbrands.com>
2013-07-26 18:09:09 -0700

    fix swarm-ui games dir source

--- swarm-ui/app/controllers/game_controller.rb
+++ swarm-ui/app/controllers/game_controller.rb
@@ -5,7 +5,7 @@ class GameController < ApplicationController
   def tick
     tick = params[:id]
 
-    game_dir  = Dir.entries("games").last
+    game_dir  = Dir.entries("../games").last
     tick_file = (tick.to_i+1).to_s.rjust(10, '0')
     path = "../games/#{game_dir}/#{tick_file}"
 
